fce4ea9928e3dd3bad698b6841b1d06c3bdec614
karthikdk <karthikdk1998@gmail.com>
2021-11-12 20:25:41 +0530

    correct ch 2 ex 8

--- _src/Chapter_02/EX2_08.c
+++ _src/Chapter_02/EX2_08.c
@@ -3,19 +3,36 @@
 #include <stdio.h>
 
 unsigned rightrot(unsigned x, int n);
+int unsignedlength(void);
 
 int main()
 {
-    unsigned x = 0xFFFF;
+    unsigned x = 99;
     
     printf("x = %d\n", x);
-    x = rightrot(x, 4);
-    printf("modified x = %d\n", x);
+    x = rightrot(x, 5);
+    printf("modified x = %i\n", x);
     
     return 0;
 }
 
 unsigned rightrot(unsigned x, int n)
 {
-    return (x >> n);            
+	int length = unsignedlength();
+	printf("bit length of x = %d\n", length);
+	
+    return ((x >> n) | (((x) & (~(~0 << n))) << (length - n)));            
+}
+
+/* bitlength: count no of bits in x */
+int unsignedlength(void)
+{
+	int b;
+	unsigned y;
+	
+	y = (~0);					  /* sets every bit of y to 1*/
+	for (b = 0; y != 0; y >>= 1)
+		b++;
+
+	return b;
 }
