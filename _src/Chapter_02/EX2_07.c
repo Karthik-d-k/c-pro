3eb995a74770bde1e9c92644fa41b966a65a06f7
karthikdk <karthikdk1998@gmail.com>
2021-11-12 06:53:32 +0530

    add ch 2 ex 7

---
+++ _src/Chapter_02/EX2_07.c
@@ -0,0 +1,25 @@
+/* Exercise 2-7. Write a function invert(x,p,n) that returns x with the n bits that begin at position p inverted (i.e., 1 changed into 0 and vice versa), 
+   leaving the others unchanged. */
+
+#include <stdio.h>
+
+unsigned invert(unsigned x, int p, int n);
+
+int main()
+{
+    unsigned x = 0xABCD;
+    
+    printf("x = %d\n", x);
+    x = invert(x, 6, 4);
+    printf("modified x = %d\n", x);
+    
+    return 0;
+}
+
+unsigned invert(unsigned x, int p, int n)
+{
+    unsigned temp = 0;
+    
+    temp = ((~(~0 << n)) << (p + 1 - n));
+    return ((x) ^ (temp));            
+}
